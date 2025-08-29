6fb87f07946775e06e8e06a977e04b6e3a3955a9
Mateusz Skalski <Mateusz.Skalski@rossmann.com.pl>
2025-08-29 12:19:00 +0200

    zmiana

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,29 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { MSAL_INSTANCE, MsalService } from '@azure/msal-angular';
+import { IPublicClientApplication, PublicClientApplication } from "@azure/msal-browser"
+import { environment } from '../environments/environment.development';
+
+export function MsalInstanceFactory(): IPublicClientApplication {
+  return new PublicClientApplication({
+    auth: {
+      clientId: environment.msal.auth.clientId,
+      redirectUri: environment.msal.auth.redirectUri
+    }
+  })
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MsalInstanceFactory
+    },
+    MsalService
   ]
 };
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    msal:{
+        auth: {
+            clientId: '359c5e12-0e9f-4077-8945-7f6e7c0b807e',
+            redirectUri: 'http://localhost:4200'
+          }
+    }
+};
